3bc7f0f89be0e2f7508023a7f6b92e08de84e61f
heiPony <1083365468@qq.com>
2020-06-17 14:40:42 +0800

    jenkins

--- src/main/java/com/pony/test/controller/TestController.java
+++ src/main/java/com/pony/test/controller/TestController.java
@@ -2,6 +2,8 @@ package com.pony.test.controller;
 
 import com.pony.test.service.AdminService;
 import com.pony.test.service.impl.AdminServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import java.util.Map;
 
 @RestController
 public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -24,6 +28,7 @@ public class TestController {
     public Map list(){
         List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from admin");
         Map<String, Object> map = maps.get(0);
+        logger.info("测试jenkins");
         return map;
     }
 
