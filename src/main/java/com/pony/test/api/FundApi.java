40e650dffbc701a361c12318863639f3cc82ee41
heiPony <1083365468@qq.com>
2020-07-07 18:19:28 +0800

    查看估值

--- src/main/java/com/pony/test/api/FundApi.java
+++ src/main/java/com/pony/test/api/FundApi.java
@@ -8,7 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/sp/fund")
 public class FundApi {
 
